c95ec64a1294b88b30036dd8cf504034e609fce0
1xAbel <111407812+1xAbel@users.noreply.github.com>
2025-08-20 23:06:54 -0400

    Update typesoulllll.lua

--- typesoulllll.lua
+++ typesoulllll.lua
@@ -1,36 +1,15 @@
-getgenv().disc_url = ''
-
-local function webHook(url, item)
-    local url = url
-
-    local data = {
-        ["username"] = "lazy hub | type soul",
-        ["content"] = "",
-        ["embeds"] = {
-            {
-                ["title"] = "afk drop",
-                ["description"] = "User : ||"..game.Players.LocalPlayer.Name.."||\nDrop : "..item,
-                ["type"] = "rich",
-                ["color"] = tonumber(0x7269da),
-            }
-        }
-    }
-
-    local newdata = game:GetService("HttpService"):JSONEncode(data)
-
-    local headers = {["content-type"] = "application/json"}
-    request = http_request or request or HttpPost
-    local webhook = {Url = url, Body = newdata, Method = "POST", Headers = headers}
-    request(webhook)
-end
-
-getgenv().outputWebhooks = true
-local lp = game.Players.LocalPlayer
-repeat task.wait() until lp.PlayerGui.ScreenEffects:WaitForChild("TextScroller")
-local textScrolling = lp.PlayerGui.ScreenEffects:WaitForChild("TextScroller")
-textScrolling.ChildAdded:Connect(function(v)
-    if getgenv().outputWebhooks then else return end
-    if v.Name == "Notification" and v.Text:match("obtained") then
-        webHook(getgenv().disc_url, v.Text)
-    end
-end)
+    'The Cero King',
+    'Unohana',
+    'The Fear',
+    'Captain of the 6th Division',
+    'Captain of the 2nd Division',
+    'The Almighty',
+    'Captain of the 12th Division',
+    'Kenpachi',
+    'The Tiger King',
+    'Captain of the 9th Division',
+    'The Bone King',
+    'The Head Captain',
+    'Captain of the 8th Division',
+    'Captain of the 3rd Division',
+    'Captain of the 7th Division',
